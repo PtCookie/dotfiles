cca97d12b51185948152e097489deee380d18369
Minsup Ju <me@ptcookie.net>
2025-06-14 00:30:49 +0900

    refactor(profile): Paths and variables
    Remove Homebrew path in .profile.
    Clean up fish config.
    Add function to highlights `fish_user_paths`.

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,46 @@
-if status is-interactive
-	# Commands to run in interactive sessions can go here
+# Path variable
+# XDG_CONFIG_HOME
+set -x ASDF_CONFIG_FILE $HOME/.config/asdf/asdfrc
+set -x ASDF_NPM_DEFAULT_PACKAGES_FILE $HOME/.config/asdf/npm-default-packages
+set -x ASDF_PYTHON_DEFAULT_PACKAGES_FILE $HOME/.config/asdf/pip-default-packages
+set -x ASDF_GEM_DEFAULT_PACKAGES_FILE $HOME/.config/asdf/gem-default-packages
+set -x NPM_CONFIG_USERCONFIG $HOME/.config/npm/npmrc
+
+# XDG_CACHE_HOME
+set -x NUGET_PACKAGES $HOME/.cache/NuGetPackage
+set -x GOMODCACHE $HOME/.cache/go/mod
+
+# XDG_DATA_HOME
+set -x DOTFILES $HOME/.local/share/dotfiles
+set -x PASSWORD_STORE_DIR $HOME/.local/share/pass
+set -x ASDF_DATA_DIR $HOME/.local/share/asdf
+set -x VAGRANT_HOME $HOME/.local/share/vagrant
+set -x COREPACK_HOME $HOME/.local/share/corepack
+set -x BUN_INSTALL $HOME/.local/share/bun
+set -x DENO_INSTALL $HOME/.local/share/deno
+set -x RUSTUP_HOME $HOME/.local/share/rustup
+set -x CARGO_HOME $HOME/.local/share/cargo
+set -x GOPATH $HOME/.local/share/go
+set -x FLUTTER_ROOT $HOME/.local/share/flutter
+
+# Android SDK path
+if test -d $HOME/Library/Android/sdk
+	set -x ANDROID_HOME $HOME/Library/Android/sdk
+else if test -d $HOME/Android/Sdk
+	set -x ANDROID_HOME $HOME/Android/Sdk
 end
+
+# Path
+fish_add_path -g /opt/homebrew/bin
+fish_add_path -g $HOME/.local/bin
+fish_add_path -g $ASDF_DATA_DIR/shims
+fish_add_path -g $HOME/.dotnet/tools
+fish_add_path -g $FLUTTER_ROOT/bin
+fish_add_path -g $CARGO_HOME/bin
+fish_add_path -g $DENO_INSTALL/bin
+fish_add_path -g $BUN_INSTALL/bin
+fish_add_path -g $ANDROID_HOME/emulator
+fish_add_path -g $ANDROID_HOME/platform-tools
 
 # Environment variable
 set -x LANG ko_KR.UTF-8
@@ -18,100 +58,59 @@ if type -q dotnet
 	set -x DOTNET_INTERACTIVE_CLI_TELEMETRY_OPTOUT true
 end
 
-# XDG_CONFIG_HOME
-set -x ASDF_CONFIG_FILE $HOME/.config/asdf/asdfrc
-set -x ASDF_NPM_DEFAULT_PACKAGES_FILE $HOME/.config/asdf/npm-default-packages
-set -x ASDF_PYTHON_DEFAULT_PACKAGES_FILE $HOME/.config/asdf/pip-default-packages
-set -x ASDF_GEM_DEFAULT_PACKAGES_FILE $HOME/.config/asdf/gem-default-packages
-set -x NPM_CONFIG_USERCONFIG $HOME/.config/npm/npmrc
-
-# XDG_CACHE_HOME
-set -x NUGET_PACKAGES $HOME/.cache/NuGetPackage
-set -x GOMODCACHE $HOME/.cache/go/mod
-
-# XDG_DATA_HOME
-set -x DOTFILES $HOME/.local/share/dotfiles
-set -x PASSWORD_STORE_DIR $HOME/.local/share/pass
-set -x VAGRANT_HOME $HOME/.local/share/vagrant
-set -x GOPATH $HOME/.local/share/go
-set -x FLUTTER_ROOT $HOME/.local/share/flutter
-set -x RUSTUP_HOME $HOME/.local/share/rustup
-set -x CARGO_HOME $HOME/.local/share/cargo
-set -x COREPACK_HOME $HOME/.local/share/corepack
-set -x DENO_INSTALL $HOME/.local/share/deno
-set -x BUN_INSTALL $HOME/.local/share/bun
-set -x ASDF_DATA_DIR $HOME/.local/share/asdf
-
-# Path
-fish_add_path -g /opt/homebrew/bin
-fish_add_path -g $HOME/.local/bin
-fish_add_path -g $HOME/.dotnet/tools
-fish_add_path -g $FLUTTER_ROOT/bin
-fish_add_path -g $CARGO_HOME/bin
-fish_add_path -g $DENO_INSTALL/bin
-fish_add_path -g $BUN_INSTALL/bin
-# Android SDK path
-if test -d $HOME/Library/Android/sdk
-	set -x ANDROID_HOME $HOME/Library/Android/sdk
-	fish_add_path -g $ANDROID_HOME/emulator
-	fish_add_path -g $ANDROID_HOME/platform-tools
-end
-fish_add_path -g $ASDF_DATA_DIR/shims
-
-# Functions
-function workdir
-	# Move to workdir
-	if set -q WORKDIR
-		cd $WORKDIR
-	else if test -d $HOME/Workspaces
-		cd $HOME/Workspaces
-	else
-		echo "Error: WORKDIR or Workspaces not found."
+if status is-interactive
+	# Functions
+	function workdir
+		# Move to workdir
+		if set -q WORKDIR
+			cd $WORKDIR
+		else if test -d $HOME/Workspaces
+			cd $HOME/Workspaces
+		else
+			echo "Error: WORKDIR or Workspaces not found."
+		end
 	end
-end
 
-function gpg-change-key
-	# Update start card status
-	if type -q gpg-connect-agent
-		gpg-connect-agent "scd serialno" "learn --force" /bye
-	else
-		echo "Error: gpg-connect-agent not found."
+	function gpg-change-key
+		# Update start card status
+		if type -q gpg-connect-agent
+			gpg-connect-agent "scd serialno" "learn --force" /bye
+		else
+			echo "Error: gpg-connect-agent not found."
+		end
 	end
-end
 
-function tcp-ports
-	# Show TCP listening ports
-	if type -q ss
-		ss -naltp
-	else
-		netstat -nap tcp | rg --color never -e "Active" -e "Address" -e "LISTEN"
+	function user-path
+		# Show PATH with fish_user_paths highlighted
+		set -x GREP_COLOR "1;34"
+		echo $PATH | grep (echo $fish_user_paths)
 	end
-end
 
-# Alias
-if type -q bat
-	alias bap='bat --paging auto'
-	alias baf='bat --style full'
-end
+	# Alias
+	if type -q bat
+		alias bap='bat --paging auto'
+		alias baf='bat --style full'
+	end
 
-if type -q lsd
-	alias l='lsd -F'
-	alias ll='lsd -lF'
-	alias la='lsd -AlF'
-	alias lsa='lsd -alF'
-end
+	if type -q lsd
+		alias l='lsd -F'
+		alias ll='lsd -lF'
+		alias la='lsd -AlF'
+		alias lsa='lsd -alF'
+	end
 
-# Launch GnuPG agent
-if type -q gpgconf
-	gpgconf --launch gpg-agent
-end
+	# Launch GnuPG agent
+	if type -q gpgconf
+		gpgconf --launch gpg-agent
+	end
 
-# Auto completion for kubectl
-if type -q kubectl
-	kubectl completion fish | source
-end
+	# Auto completion for kubectl
+	if type -q kubectl
+		kubectl completion fish | source
+	end
 
-# Starship prompt initialization
-if type -q starship
-	starship init fish | source
+	# Starship prompt initialization
+	if type -q starship
+		starship init fish | source
+	end
 end
